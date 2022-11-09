ef8fd088404e906b020c9f9963c47285bb101b6f
Marco Mendão <mac.mendao@gmail.com>
2022-11-09 12:15:24 +0000

    Add logic

--- 08_logic.py
+++ 08_logic.py
@@ -1,4 +1,4 @@
-# If <condition> then <instruction> else <instruction>
+# If <condition> then <login> else <logic>
 
 augusto = {
     "name": "Augusto",
@@ -28,3 +28,67 @@ elif person["size"] >= 1.6 and person["size"] < 2:
     print(f"{person['name']} is average")
 else:
     print(f"{person['name']} is short")
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# ...
+
+# Haja paciencia
+# Do not use, use a loop instead
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+
+
+## Loops
+
+# while <Condition> do <logic>
+count = 1
+while count <= 10:
+    print(count)
+    count = count + 1
+
+## While True, suggest by Fernando Paz
+while True:
+    print(count)
+    count = count + 1
+    if count >= 10:
+        break
+
+people = [pedro, augusto, pedro]
+
+i = 0
+
+while i < len(people):
+    current_person = people[i]
+    print(f"people[{i}] =",current_person["name"])
+    i += 1
+
+# 1 IT 
+# i - 0
+# current_person = people[0] = pedro
+
+# 2 IT
+# i - 1
+# current_person = people[1] = augusto
+
+# 3 IT
+# i - 2
+# i < len(people) = 2 < len(people) = False = Break Loop
+
+# for <current_iteration> in <list_of_values>
+
+for current_person in people:
+    print(current_person["name"])
+
+# IT 1
+# current_person = pedro
+
+# IT 2
+# current_person = augusto
