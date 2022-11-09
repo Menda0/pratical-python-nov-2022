9ea36995811d6f2450b06018ad994807c1841907
Marco Mendão <mac.mendao@gmail.com>
2022-11-09 12:40:35 +0000

    Add exercises 4

---
+++ 09_exercises4.py
@@ -0,0 +1,11 @@
+
+# 1. Create a list of 6 people
+# Example jonh = ...
+# 2. People mas have the following attributes
+# Example: {name:"Jonh Doe", size: 1.34, age:18}
+# 3. Iterate over the list of people and create a new list with the people over 18 years old
+# Example: adults = [...] if age >= 18
+# 4. Iterate over the list of people and a create 3 lists, short_people, average_people and tall people
+# Example: short_people = [...] if size < 1.6
+# 5. Print all the list created
+# Example: print(short_people)
