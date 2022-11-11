f196dae6f05d8ca7229137f3633de12fb615c49d
Marco Mendão <mac.mendao@gmail.com>
2022-11-11 12:32:37 +0000

    Add exercise 13-2

--- 13_exercises6.py
+++ 13_exercises6.py
@@ -8,3 +8,16 @@
 # constructor -> the athlete must have 4 points of energy
 # 4. Create/Define a new __repr__ function that prints the athlete name and energy points
 # 5. Try to run and eat something with the athlete, print the athlete whenever your preform an action
+
+
+# 1. Create a class Animal with attributes:
+# type -> mamal, bird, reptil, etc
+# name -> Bear, Dog, Cat, Parrot
+# noise -> noise that the animal makes
+# 2. Create a method called speak. That prints the noise of the animal
+# (Example: Miau, Auaua, Pipiu)
+# 3. Create a subclass that represents a type of animal
+# (Example: Bird, Mamal, Reptil)
+# 4. Create 6 animals instances of multiple types
+# (Example: mamal_dog, reptil_cobra, bird_eagle)
+# 5. Make everyone speak
